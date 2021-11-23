79cfe17412bff0d5180e787cc1c02cd2516301ef
shakil89427 <shakilahmed89427@gmail.com>
2021-11-24 00:55:29 +0600

    header and banner done

---
+++ src/Components/Header/Header.js
@@ -0,0 +1,60 @@
+import React from "react";
+import { Container, Nav, Navbar } from "react-bootstrap";
+import "./Header.css";
+import logo from "../../images/logo.png";
+import { Link } from "react-router-dom";
+
+const Header = () => {
+  return (
+    <Navbar collapseOnSelect expand="lg" className="navbar">
+      <Container>
+        <Navbar.Brand href="#home">
+          <img className="me-3" src={logo} alt="" />
+          <p className="fs-6 d-inline">
+            <small>Welcome, Shakil Ahmed</small>
+          </p>
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse className="text-center" id="responsive-navbar-nav">
+          <Nav className="ms-auto ">
+            <div className="d-flex justify-content-center align-items-center">
+              <Link
+                to="home"
+                className="text-decoration-none text-dark fw-bolder me-2"
+              >
+                Home
+              </Link>
+              <Link
+                to="home"
+                className="text-decoration-none text-dark fw-bolder me-2"
+              >
+                Team
+              </Link>
+              <Link
+                to="home"
+                className="text-decoration-none text-dark fw-bolder me-2"
+              >
+                Career
+              </Link>
+              <Link
+                to="home"
+                className="text-decoration-none text-dark fw-bolder me-2"
+              >
+                Blog
+              </Link>
+
+              <Link
+                to="home"
+                className="text-decoration-none text-dark fw-bolder me-2"
+              >
+                Login
+              </Link>
+            </div>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+};
+
+export default Header;
---
+++ src/Components/Home/Banner/Banner.js
@@ -0,0 +1,66 @@
+import React from "react";
+import { Col, Container, Row } from "react-bootstrap";
+import logo from "../../../images/mobile.png";
+import "./Banner.css";
+
+const Banner = () => {
+  return (
+    <div>
+      <div className="banner ">
+        <Container>
+          <Row className="text-center">
+            <Col className="py-3 my-auto text-white" sm={12} md={6} lg={6}>
+              <h1>Showcase your app</h1>
+              <h1>with Small Apps</h1>
+              <p>
+                Besides its beautiful design. Laapp is an incredibly rich core
+                framework for you to showcase your App.
+              </p>
+              <button className="banner-btn">DOWNLOAD NOW</button>
+            </Col>
+            <Col className="py-5" sm={12} md={6} lg={6}>
+              <img className="img-fluid" src={logo} alt="" />
+            </Col>
+          </Row>
+        </Container>
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 170">
+          <path
+            fill="#ffffff"
+            fill-opacity="1"
+            d="M0,64L120,96C240,128,480,192,720,192C960,192,1200,128,1320,96L1440,64L1440,320L1320,320C1200,320,960,320,720,320C480,320,240,320,120,320L0,320Z"
+          ></path>
+        </svg>
+      </div>
+      <Container className="bottom-banner mx-auto text-center p-3">
+        <Row>
+          <Col sm={12} md={6} lg={4}>
+            <i className="fs-2 mb-4 text-info fas fa-coffee"></i>
+            <h5>Themes Made Easy</h5>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam
+              non, recusandae tempore ipsam dignissimos molestias.
+            </p>
+          </Col>
+          <Col sm={12} md={6} lg={4}>
+            <i className="fs-2 mb-4 text-info fas fa-sun"></i>
+            <h5>Powerful Design</h5>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam
+              non, recusandae tempore ipsam dignissimos molestias.
+            </p>
+          </Col>
+          <Col sm={12} md={12} lg={4}>
+            <i className="fs-2 mb-4 text-info fas fa-cloud"></i>
+            <h5>Creative Content</h5>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam
+              non, recusandae tempore ipsam dignissimos molestias.
+            </p>
+          </Col>
+        </Row>
+      </Container>
+    </div>
+  );
+};
+
+export default Banner;
